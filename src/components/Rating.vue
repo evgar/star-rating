bc46d6ad20087815993c1a0ce1802ce8419b4c69
eugene garichev <e.garichev@viseven.com>
2018-03-16 18:15:12 +0200

    update(Rating.vue):update component

--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="rating">
     <ul class="rating__list">
-      <li class="list__item" v-for="rate in maxRating">
-        <icon class="list__icon" name="star"></icon>
+      <li class="list__item" v-for="(rate, index) in maxRating" @click="onClick(index)" @mouseover="onHover(index)" @mouseleave="onMouseLeave">
+        <icon class="list__icon" :name="index >= rating ? 'star-o' : 'star'"></icon>
       </li>
     </ul>
   </div>
@@ -16,25 +16,42 @@
   export default {
     data() {
       return {
-        rating: 3,
-        maxRating: 5
+        rating: 0,
+        maxRating: 5,
+        initialRating: 0
       }
     },
-    components: { Icon }
+    components: { Icon },
+    methods: {
+      onClick: function(index) {
+        this.rating = index + 1;
+        this.initialRating = this.rating;
+      },
+      onHover: function(index) {
+        this.rating = index + 1;
+      },
+      onMouseLeave: function() {
+        this.rating = this.initialRating;
+      }
+    }
   }
 </script>
 
 <style scoped>
   .rating {
-    
-  }
-  .rating__list {
 
   }
+  .rating__list {
+    display: flex;
+    list-style: none;
+  }
   .list__item {
+    padding-right: 15px;
+    cursor: pointer;
 
   }
   .list__icon {
-
+    color: gold;
   }
+
 </style>
